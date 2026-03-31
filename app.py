6342a7b2e972ab96996cfeb202a4a898a7d366c5
Yeu <yeumensilva9@gmail.com>
2026-03-31 20:45:41 -0300

    Fix Format

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 st.set_page_config(page_title="Procesador de CSV", page_icon="📊")
 
-st.title("🚀 Procesador de CSV AFIP")
-st.write("Sube tu archivo y descarga el resultado con formato Unix (LF).")
+st.title("Procesador de CSV ARCA")
+st.write("Subí tu archivo.")
 
 uploaded_file = st.file_uploader("Elegir archivo CSV", type=['csv'])
 
@@ -58,6 +58,8 @@ if uploaded_file is not None:
         
         st.info("Vista previa de los datos procesados:")
         st.dataframe(df.head())
+
+        st.write("Yeu y Martin")
 
     except Exception as e:
         st.error(f"Hubo un error: {e}")
